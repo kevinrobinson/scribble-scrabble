c7b22463492f4424305e65b9bd42381eb380203a
kevinrobinson <kevin.robinson.0@gmail.com>
2020-04-29 18:18:20 -0400

    Update SSL again

--- server/db.js
+++ server/db.js
@@ -5,9 +5,18 @@ import {Pool} from 'pg';
 // For querying the database
 function createPool() {
   const {DATABASE_URL, NODE_ENV} = getConfig().serverRuntimeConfig;
-  // re ssl, see https://github.com/brianc/node-postgres/issues/2009#issuecomment-615484484
+  
+  // re ssl...
+  // see https://help.heroku.com/3DELT3RK/why-can-t-my-third-party-utility-connect-to-heroku-postgres-with-ssl
+  // and https://github.com/brianc/node-postgres/issues/2009#issuecomment-615484484
+  // and https://security.stackexchange.com/a/229297
+  // and review Heroku env variables
+  const ssl = (NODE_ENV === 'development')
+    ? false
+    : { rejectUnauthorized: false };
   return new Pool({
-    connectionString: DATABASE_URL
+    connectionString: DATABASE_URL,
+    ssl
   });
 }
 
